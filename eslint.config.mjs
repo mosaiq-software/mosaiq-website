e230aa1b8e434a5b6bbfb421dbd4c9010d628d52
Matt <47070087+Camo651@users.noreply.github.com>
2025-09-25 14:14:30 -0400

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ["**/*.{ts,tsx,astro}"] },
+    { files: ['**/*.{ts,tsx,astro}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     {
         rules: {
-            eqeqeq: "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-            "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-            "@typescript-eslint/no-empty-object-type": "off",
-        }
+            eqeqeq: 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+            '@typescript-eslint/no-empty-object-type': 'off',
+        },
     },
     {
-        ignores: [".node_modules/*", "dist/*", "scripts/*", "*.cjs", "public/*"]
+        ignores: ['.node_modules/*', 'dist/*', 'scripts/*', '*.cjs', 'public/*'],
     },
 ];
